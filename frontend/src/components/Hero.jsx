7cbfe56fc9e1a55ed25ce7f7b55315a6ef92ba47
devanshupatil <devanshupatil692@gmail.com>
2025-06-09 23:28:50 +0530

    updates

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ArrowRight, Sparkles, Users } from 'lucide-react';
 import biodataImage_1 from '../assets/images/_images_examples_eg12.png';
 import biodataImage_2 from '../assets/images/_images_examples_eg20.png';
@@ -6,6 +6,22 @@ import biodataImage_3 from '../assets/images/_images_examples_eg23.png';
 
 
 const Hero = () => {
+
+  const [isPhone, setIsPhone] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsPhone(window.innerWidth < 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup listener on unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <section id="home" className="py-12 md:py-30 bg-gradient-to-b from-white to-gray-50 ">
       <div className="container mx-auto px-4">
@@ -30,38 +46,48 @@ const Hero = () => {
             </div>
           </div>
           <div className="lg:w-1/2 relative">
-            <div className="relative w-full max-w-2xl mx-auto h-[300px] hover:scale-105 transition-all duration-500">
 
-              {/* Template 1 - Back */}
-              <div className="absolute top-6 left-10 transform -rotate-10">
-                <img
-                  src={biodataImage_2}
-                  alt="Marriage Biodata Templates"
-                  className="w-[200px] h-auto rounded-lg shadow-xl"
-                />
+            {isPhone ? (
+              <div>
+                <img src={biodataImage_1} alt="Marriage Biodata Templates" className="w-full h-auto rounded-lg shadow-xl" />
               </div>
 
-              {/* Template 2 - Middle */}
-              <div className="absolute top-6 right-10 transform rotate-10">
+            ) : (
+              <div className="relative w-full max-w-2xl mx-auto h-[300px] hover:scale-105 transition-all duration-500">
 
-                <img
-                  src={biodataImage_1}
-                  alt="Marriage Biodata Templates"
-                  className="w-[200px] h-auto rounded-lg shadow-xl"
-                />
+                {/* Template 1 - Back */}
+                <div className="absolute top-6 left-10 transform -rotate-10">
+                  <img
+                    src={biodataImage_2}
+                    alt="Marriage Biodata Templates"
+                    className="w-[200px] h-auto rounded-lg shadow-xl"
+                  />
+                </div>
+
+                {/* Template 2 - Middle */}
+                <div className="absolute top-6 right-10 transform rotate-10">
+
+                  <img
+                    src={biodataImage_1}
+                    alt="Marriage Biodata Templates"
+                    className="w-[200px] h-auto rounded-lg shadow-xl"
+                  />
+                </div>
+
+                {/* Template 3 - Front */}
+                <div className="absolute top-0 left-50 transform">
+
+                  <img
+                    src={biodataImage_3}
+                    alt="Marriage Biodata Templates"
+                    className="w-[220px] h-auto rounded-lg shadow-xl"
+                  />
+                </div>
+
               </div>
 
-              {/* Template 3 - Front */}
-              <div className="absolute top-0 left-50 transform">
+            )}
 
-                <img
-                  src={biodataImage_3}
-                  alt="Marriage Biodata Templates"
-                  className="w-[220px] h-auto rounded-lg shadow-xl"
-                />
-              </div>
-
-            </div>
           </div>
         </div>
       </div>
